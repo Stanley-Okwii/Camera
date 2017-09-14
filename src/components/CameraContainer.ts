cdf2011ac513729daa3e1dee058ae5626dd33946
Stanley-Okwii <stanleeparker12@gmail.com>
2017-09-14 14:41:05 +0300

    Add retake and Use picture buttons

--- src/components/CameraContainer.ts
+++ src/components/CameraContainer.ts
@@ -1,8 +1,9 @@
 import { Component, createElement } from "react";
 import { Alert } from "./Alert";
 import * as WebCam from "react-webcam";
+import * as ReactDOM from  "react-dom";
 import { cameraButton, BootstrapStyle } from "./Camera";
-
+//var ReactDOM = require('react-dom');
 interface WrapperProps {
     class: string;
     mxObject: mendix.lib.MxObject;
@@ -20,12 +21,13 @@ interface ContainerState {
     cameraState: any;
     screenshot: any;
     active: boolean;
+    buttonState: any;
 
 }
 
 export default class CameraContainer extends Component<ContainerProps, ContainerState> {
     private subscriptionHandles: number[];
-    private webcam = WebCam;
+    private webcam: any;
     constructor(props: ContainerProps) {
         super(props);
 
@@ -33,6 +35,7 @@ export default class CameraContainer extends Component<ContainerProps, Container
             screenshot: null,
             active: false,
             cameraState: null,
+            buttonState: null
         };
 
         this.setRef = this.setRef.bind(this);
@@ -45,39 +48,74 @@ export default class CameraContainer extends Component<ContainerProps, Container
         let activateButtonElement;
         let captureButton;
         let picturetaken;
+        let cameraNode;
+        let buttonNode;
+        let buttonNode2;
+        let retakePictureButton;
+        let usePictureButton;
+        let cameraNode2;
+        cameraElement = createElement(WebCam, {
+            audio: false,
+            ref: this.setRef,
+            screenshotFormat: "image/jpeg",
+            height: this.props.resolutionHeight,
+            width: this.props.resolutionWidth
+        });
+        captureButton = createElement(cameraButton, {
+            bootstrapStyle: "btn btn-primary",
+            onClickAction: this.takePicture,
+            style: CameraContainer.parseStyle(this.props.style),
+            type: "button",
+            value: "Take picture"
+        });
+        buttonNode2 = createElement("div", {
+            className: ""
+        }, retakePictureButton, usePictureButton
+        );
+        retakePictureButton = createElement(cameraButton, {
+            bootstrapStyle: "btn btn-primary",
+            onClickAction: this.startCamera,
+            style: CameraContainer.parseStyle(this.props.style),
+            type: "button",
+            value: "Retake"
+        });
+         usePictureButton = createElement(cameraButton, {
+            bootstrapStyle: "btn btn-primary",
+            //onClickAction: this.startCamera,
+            style: CameraContainer.parseStyle(this.props.style),
+            type: "button",
+            value: "Use Picture"
+        });
+
+        picturetaken = createElement("img", {
+            src: this.state.screenshot,
+            style: CameraContainer.parseStyle(this.props.style),
+            alt: "image not found"
+        });
+        buttonNode = createElement("div", {
+            className: ""
+        }, captureButton);
+        buttonNode2 = createElement("div", { className: "" }, retakePictureButton, usePictureButton);
+        cameraNode = createElement("div", { className: "" }, cameraElement, buttonNode);
+        cameraNode2 = createElement("div", { className: "" }, picturetaken, buttonNode2);
+
         if (this.state.active) {
-            cameraElement = createElement(this.webcam, {
-                audio: false,
-                ref: this.setRef,
-                screenshotFormat: "image/jpeg",
-                height: this.props.resolutionHeight,
-                width: this.props.resolutionWidth
-            });
-            captureButton = createElement(cameraButton, {
-                bootstrapStyle: "btn btn-primary",
-                onClickAction: this.takePicture,
-                style: CameraContainer.parseStyle(this.props.style),
-                type: "button",
-                value: "Take picture"
-            });
+            this.setState({ cameraState: cameraNode });
             if (this.state.screenshot) {
-                picturetaken = createElement("img", {
-                    src: this.state.screenshot,
-                    style: CameraContainer.parseStyle(this.props.style),
-                    alt: "image not found"
-                });
+                this.setState({ cameraState: cameraNode2 });
+                //this.setState({buttonState: null});
             }
         } else {
-            activateButtonElement = createElement(cameraButton, {
-                bootstrapStyle: "btn btn-primary",
-                onClickAction: this.startCamera,
-                style: CameraContainer.parseStyle(this.props.style),
-                type: "button",
-                value: "Activate Camera"
-            });
+           // this.setState({ buttonState: activateButtonElement });
+           activateButtonElement = createElement(cameraButton, {
+            bootstrapStyle: "btn btn-primary",
+            onClickAction: this.startCamera,
+            style: CameraContainer.parseStyle(this.props.style),
+            type: "button",
+            value: "Activate Camera"
+        });
         }
-
-        return createElement("div", { className: "" }, activateButtonElement, cameraElement, picturetaken, captureButton);
+        return createElement("div", { className: "" }, activateButtonElement, this.state.cameraState);
     }
     componentWillReceiveProps(nextProps: ContainerProps) {
 
@@ -86,13 +124,15 @@ export default class CameraContainer extends Component<ContainerProps, Container
     componentWillUnmount() {
 
     }
+
     private setRef(webcam: any) {
-        this.webcam = webcam; 
+        this.webcam = webcam;
     }
 
     private takePicture() {
         const screenshot1 = this.webcam.getScreenshot();
         this.setState({ screenshot: screenshot1 });
+        //this.setState({ active:false });
     }
 
     private startCamera() {
--- typings/camera.d.ts
+++ typings/camera.d.ts
@@ -1,9 +1,7 @@
-
-declare module "canvas-to-blob";
 declare module "react-webcam";
 declare module "react-dom";
-declare const require: {
-    <T>(path: string): T;
-    (paths: string[], callback: (...modules: any[]) => void): void;
-    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
-}
+// declare const require: {
+//     <T>(path: string): T;
+//     (paths: string[], callback: (...modules: any[]) => void): void;
+//     ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
+// }
